5a5360772f8bdce865f3ed52cdb112c1e2c92e72
ex404aditya <ex404ug@gmail.com>
2024-12-01 04:53:40 +0530

    test47

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,21 +1,29 @@
 import { auth } from "@hypermode/modus-sdk-as";
-import { getCurrentUser } from "./user";
-import { getWorkspaces, getWorkspaceNotes, createWorkspace, createNote, updateNote, deleteNote, getNote, verifyWorkspaceAccess } from "./workspace";
-import { Workspace } from "./models";
-
-export function sayHello(name: string): string {
-  const user = getCurrentUser();
-  return `Hello ${user.id} ${user.clerkid} ${user.firstname} ${user.lastname}!`;
-}
-
-export { 
-  getCurrentUser,
-  getWorkspaces, 
+// import { getCurrentUser } from "./user";
+import {
+  getWorkspaces,
   getWorkspaceNotes,
   createWorkspace,
   createNote,
   updateNote,
   deleteNote,
   getNote,
-  verifyWorkspaceAccess
+  verifyWorkspaceAccess,
+} from "./workspace";
+import { Workspace } from "./models";
+
+// export function sayHello(name: string): string {
+//   const user = getCurrentUser();
+//   return `Hello ${user.id} ${user.clerkid} ${user.firstname} ${user.lastname}!`;
+// }
+
+export {
+  getWorkspaces,
+  getWorkspaceNotes,
+  createWorkspace,
+  createNote,
+  updateNote,
+  deleteNote,
+  getNote,
+  verifyWorkspaceAccess,
 };
